400f2985ef969a185404ed4d54f74208ba145ed3
Bharadwaj P <barbi.bruce@gmail.com>
2011-06-11 01:05:34 -0500

    added active boolean to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110611051723) do
+ActiveRecord::Schema.define(:version => 20110611060437) do
 
   create_table "jobs", :force => true do |t|
     t.string   "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20110611051723) do
     t.datetime "updated_at"
     t.string   "encrypted_password"
     t.integer  "token"
+    t.boolean  "active"
   end
 
 end
